67cd7e716582bb3793dede2c06efb4a33b56190a
Jason Shum <jshum@cs.brown.edu>
2013-04-08 21:42:01 -0400

    5 in progress

--- denoiseQuadratic.m
+++ denoiseQuadratic.m
@@ -2,4 +2,12 @@ function x=denoiseQuadratic(inp,k)
 % Given an input image inp, and a scaling factor k, returns the optimal
 % quadratic denoising of the image, as specified in the handout
 
+x = zeros ()
 
+
+kernel = [0 1 0; 1 4 1; 0 1 0];
+for i=1:size(inp, 1)
+    for j=1:size(inp,2)
+        i
+    end
+end
--- tester.m
+++ tester.m
@@ -6,10 +6,18 @@ clc
 % wideScaleRandomNoiseProposal([0 0])
 
 % arrangeCircles(1:20,0)
-
-x = localSearchCircle(@arrangeCircles, @wideScaleRandomNoiseMix3Proposal, 1:20, 0, 0, 100, 10, 0.01);
+% 
+% x = localSearchCircle(@arrangeCircles, @wideScaleRandomNoiseMix3Proposal, 1:20, 0, 0, 100, 10, 0.01);
 %%
-x=reshape(x,2,10);
-width=max(x(1,:)+(1:10))-min(x(1,:)-(1:10));
-height=max(x(2,:)+(1:10))-min(x(2,:)-(1:10));
-rating=max(width,height);
+% x=reshape(x,2,10);
+% width=max(x(1,:)+(1:10))-min(x(1,:)-(1:10));
+% height=max(x(2,:)+(1:10))-min(x(2,:)-(1:10));
+% rating=max(width,height);
+% 
+
+%%
+
+inputimg = double(imread('lenaNoise.jpg'));
+
+fixedimg = denoiseQuadratic(inputimg, 2);
+
